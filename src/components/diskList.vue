99c6180cbf61b2be39084f16ccda8aa7b89b5629
Vegadrone <spenaantonino@gmail.com>
2022-07-19 16:18:31 +0200

    Import data from axios

--- src/components/diskList.vue
+++ src/components/diskList.vue
@@ -6,16 +6,38 @@
 </template>
 
 <script>
+import axios from "axios";
 import diskCard from "./diskCard.vue";
 
 export default {
- components: {
+  data: function () {
+    return {
+        disks: [],
+    };
+  },
+  components: {
     diskCard,
   },
-}
+  methods: {
+    getDisk() {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((result)=>{
+            console.log(result.data.response)
+            this.disks.push(result.data.response)
+        })
+        .catch((error)=>{
+            console.warn(error)
+
+        })
+    },
+  },
+    
+  created() {
+    this.getDisk();
+  },
+};
 </script>
 
 <style lang="scss">
 @import "../assets/styles/variables.scss";
-
 </style>
