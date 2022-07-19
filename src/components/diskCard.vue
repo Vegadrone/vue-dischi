0b5bd622ac8347ddccc5b3f92bce1c0589805b79
Vegadrone <spenaantonino@gmail.com>
2022-07-19 16:53:54 +0200

    Add immages

--- src/components/diskCard.vue
+++ src/components/diskCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card-wrapper">
-    <img src="disk.poster" alt="disk.title">
+    <img :src="disk.poster" :alt="disk.title">
     <div class="card-text text-center">
       <h5 class="title text-light fw-bold">{{disk.title}}</h5>
       <span class="artist fw-bold">{{disk.author}}</span>
